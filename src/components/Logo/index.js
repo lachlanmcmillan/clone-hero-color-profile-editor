f6e615285b3fd5ed5b980ca251d30dc315ae3cfc
Lachlan McMillan <lachlanmcmillan@users.noreply.github.com>
2022-05-20 17:58:26 +1000

    update styles

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import styles from "./Logo.module.css";
 
 export const Logo = (props) => (
-  <div className={styles.logo}>
-    {/* <img className={styles.img} src="/images/ch.png" /> */}
-    <h1 className={styles.text}>Clone Hero Note Editor</h1>
-  </div>
+  <h1 className={styles.logo}>
+    Clone Hero Color Editor
+  </h1>
 );
